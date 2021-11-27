1d2781c414da440234d861811cfa880637b94674
Valerii Kalenyuk <vakabo@gmail.com>
2021-11-27 22:41:32 +0300

    added contacts functionality

---
+++ contacts.js
@@ -0,0 +1,49 @@
+import fs from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url'; // in Browser, the URL in native accessible on window
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+const contactsPath = path.resolve('db', 'contacts.json');
+
+export async function listContacts() {
+  const contacts = (await fs.readFile(contactsPath)).toString();
+  return JSON.parse(contacts);
+}
+
+export async function getContactById(contactId) {
+  const contacts = await listContacts();
+  return (
+    contacts.find(contact => contact.id === contactId) || 'no such contact'
+  );
+}
+
+export async function removeContact(contactId) {
+  const contacts = await listContacts();
+
+  const check = contacts.find(contact => contact.id === contactId);
+
+  if (!check) {
+    console.log('no such contact');
+    return;
+  }
+
+  const updContacts = contacts.filter(contact => contact.id !== contactId);
+
+  fs.writeFile(contactsPath, JSON.stringify(updContacts));
+}
+
+export async function addContact(name, email, phone) {
+  const contact = { name, email, phone };
+  const contacts = await listContacts();
+
+  for (let i = 0; i < contacts.length + 1; i++) {
+    if (contacts[i]?.id !== i + 1) {
+      contacts.push({ id: i + 1, ...contact });
+      break;
+    }
+  }
+
+  fs.writeFile(contactsPath, JSON.stringify(contacts));
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from './contacts.js';
+
+// const contacts = await listContacts();
+// console.log(contacts);
+
+// console.log(await getContactById(6));
+// removeContact(11);
+addContact('Valerii Mango', 'asdf@asd.com', '(126) 458-4865');
+
+// listContacts();
+
+// {
+//   id: 6,
+//   name: 'Abbot Franks',
+//   email: 'scelerisque@magnis.org',
+//   phone: '(186) 568-3720'
+// }
